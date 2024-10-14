c0e8af951cf7cf744b074c85fd8eecbf7690a17d
sanika gore <sanikavgore2020@gmail.com>
2024-10-14 18:36:25 +0530

    week 4

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ document.querySelector('.button a').addEventListener('click', function() {
     this.classList.add('active');
 });
 
-// Add event listener to the products
+// Add event listener to the Products
 document.querySelectorAll('.products .card').forEach(function(card) {
     card.addEventListener('mouseover', function() {
         // Add class to the card
